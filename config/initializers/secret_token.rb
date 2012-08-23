87e1dec3f4499a96b60c1b38efc773c75ae6cbf5
Ryan Thompson <ryan.glenn.thompson@gmail.com>
2012-08-23 16:19:06 -0500

    create new rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e15093bb57444f69741380bd0603c724e0e56fb129b470b9ed415893fa35aa8187b50a1a231168cf8ee0c0cd9dad1ca5b1e77adca8725c88c4ea3baf13a84bba'
